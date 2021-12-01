88ec44356a4292c457e95aff940ff3f5f6ef84f5
Jamie Holding <me@jamie.holdings>
2021-12-01 06:42:50 +0000

    [+] Day 1 Part 1

--- 01.js
+++ 01.js
@@ -4,7 +4,14 @@ const Advent = new AdventLib(1, 2021);
 async function Run() {
     const input = await Advent.GetInput();
 
-    // await Advent.Submit(null);
+    const ans1 = input.map(Number).reduce((p, n, idx, arr) => {
+        if (idx > 0 && n > arr[idx - 1]) {
+            return p + 1;
+        }
+        return p;
+    }, 0);
+
+    await Advent.Submit(ans1);
     // await Advent.Submit(null, 2);
 }
 Run();
