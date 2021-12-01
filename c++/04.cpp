fe5e0d806597c5880bce1289f3b22027fb734e16
Jamie Holding <me@jamie.holdings>
2021-12-01 08:05:12 +0000

    [-] old c++ stuff

--- c++/04.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <iostream>
-#include "solutions.h"
-
-class Day04 : public SolutionInterface<Day04> {
-public:
-};
--- c++/05.cpp
+++
@@ -1,8 +0,0 @@
-
-#include <iostream>
-#include "solutions.h"
-
-class Day05 : public SolutionInterface<Day05> {
-public:
-    
-};
