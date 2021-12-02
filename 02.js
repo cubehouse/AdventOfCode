f298a2d10aafdded24162825b10e697f26d33c8c
Jamie Holding <me@jamie.holdings>
2021-12-02 05:46:28 +0000

    [~] Rewrite day 2 with a generic opmachine * Often need something similar to this, so why not build it now while I get baby to sleep

--- 02.js
+++ 02.js
@@ -1,8 +1,13 @@
 import { Advent as AdventLib } from './lib/advent.js';
 const Advent = new AdventLib(2, 2021);
 
+import { OpMachine } from './lib/opmachine.js';
+
 async function Run() {
     const input = await Advent.GetInput();
+
+/*
+    // Original Solution
 
     const instr = input.map((x) => {
         const s = x.split(' ');
@@ -44,6 +49,47 @@ async function Run() {
             throw new Error('unknown op ' + x.op);
         }
     });
+
+    await Advent.Submit(horz * depth, 2);
+*/
+
+    // using new OpMachine class to try and help speed up puzzles like this one
+
+    // part 1
+    const opMachine = new OpMachine();
+    opMachine.LoadProgram(input, OpMachine.OpAndNumbers);
+    let horz = 0;
+    let depth = 0;
+    opMachine.on('op_forward', (val) => {
+        horz += val;
+    });
+    opMachine.on('op_up', (val) => {
+        depth -= val;
+    });
+    opMachine.on('op_down', (val) => {
+        depth += val;
+    });
+    opMachine.Run();
+    
+    await Advent.Submit(horz * depth);
+
+    // part 2
+    const opMachine2 = new OpMachine();
+    opMachine2.LoadProgram(input, OpMachine.OpAndNumbers);
+    horz = 0;
+    depth = 0;
+    let aim = 0;
+    opMachine2.on('op_forward', (val) => {
+        horz += val;
+        depth += val * aim;
+    });
+    opMachine2.on('op_up', (val) => {
+        aim -= val;
+    });
+    opMachine2.on('op_down', (val) => {
+        aim += val;
+    });
+    opMachine2.Run();
 
     await Advent.Submit(horz * depth, 2);
 }
