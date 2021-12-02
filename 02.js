36e4b3a5452d31527c0d9f1a50b43f3f2eb424f5
Jamie Holding <me@jamie.holdings>
2021-12-02 05:07:10 +0000

    [+] Day 2

--- 02.js
+++ 02.js
@@ -4,7 +4,47 @@ const Advent = new AdventLib(2, 2021);
 async function Run() {
     const input = await Advent.GetInput();
 
-    // await Advent.Submit(null);
-    // await Advent.Submit(null, 2);
+    const instr = input.map((x) => {
+        const s = x.split(' ');
+        return {
+            op: s[0],
+            val: Number(s[1]),
+        };
+    });
+
+    let horz = 0;
+    let depth = 0;
+    instr.forEach((x) => {
+        if (x.op === 'forward') {
+            horz += x.val;
+        } else if (x.op === 'down') {
+            depth += x.val;
+        } else if (x.op === 'up') {
+            depth -= x.val;
+        } else {
+            throw new Error('unknown op ' + x.op);
+        }
+    });
+
+    await Advent.Submit(horz * depth);
+
+    let aim = 0;
+    horz = 0;
+    depth = 0;
+
+    instr.forEach((x) => {
+        if (x.op === 'forward') {
+            horz += x.val;
+            depth += x.val * aim;
+        } else if (x.op === 'down') {
+            aim += x.val;
+        } else if (x.op === 'up') {
+            aim -= x.val;
+        } else {
+            throw new Error('unknown op ' + x.op);
+        }
+    });
+
+    await Advent.Submit(horz * depth, 2);
 }
 Run();
