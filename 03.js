d95f5f75766df47fbb7172060e7ee14830f760fe
Jamie Holding <me@jamie.holdings>
2021-12-03 17:32:33 +0000

    [+] Day 3 part 1

--- 03.js
+++ 03.js
@@ -4,7 +4,24 @@ const Advent = new AdventLib(3, 2021);
 async function Run() {
     const input = await Advent.GetInput();
 
-    // await Advent.Submit(null);
+    const bits = input[0].length;
+    const inputs = input.length;
+    const inputsHalf = inputs / 2;
+
+    // largest binary number possible
+    const maxInt = parseInt(new Array(bits).fill('1').join(''), 2);
+
+    const bitSums = (new Array(bits).fill('')).map((_, bit) => {
+        const a = input.reduce((p, x) => {
+            return p + ((x.slice(bit, bit + 1) === '1') ? 1 : 0);
+        }, 0);
+        return (a >= inputsHalf) ? 1 : 0;
+    });
+    const gamma = parseInt(bitSums.join(''), 2);
+    
+    // epsilon is largest int - gamma
+    await Advent.Submit(gamma * (maxInt - gamma));
+
     // await Advent.Submit(null, 2);
 }
 Run();
