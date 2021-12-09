e8481d2d92bb257a31d5b36c763792691e44ffa1
Jamie Holding <me@jamie.holdings>
2021-12-09 06:08:19 +0000

    [~] Day 9, make visualisation dynamic so we follow the map view

--- 09.js
+++ 09.js
@@ -11,7 +11,7 @@ async function Run() {
 8767896789
 9899965678`.split('\n');
 
-    const S = new Screen();
+    const S = new Screen({ logWidth: 25 });
 
     const map = input.map((line) => {
         return line.split('').map((c) => Number(c));
@@ -47,38 +47,57 @@ async function Run() {
 
     await Advent.Submit(lowPointsTotal);
 
+    // part 2
+
+    // dump input into a 2d grid
     S.Set(0, 0, input.join('\n'));
 
+    // parse height from each cell and store in cell as 'height'
     S.ForEachCell((x) => {
         S.SetKey(x.x, x.y, 'height', Number(x.val));
     });
 
+    // some flavour to the visual map
     S.AddStyle(/(X)/, '{green-fg}');
     S.AddStyle(/(\.)/, '{green-fg}');
-
     lowPoints.forEach((point) => {
         S.Set(point.col, point.row, `X`);
     });
 
+    let maxY = 0;
+
+    // flood fill each lowest point from part 1
     for (const point of lowPoints) {
         let count = 0;
         await S.FloodFill(point.col, point.row, (x) => {
             return x?.height !== undefined && x?.height < 9;
         }, (x) => {
             S.Set(x.x, x.y, '.');
+
+            // shift screen view to fit in the new areas of the map
+            if (x.y > maxY) {
+                maxY = x.y;
+                S.FitPositionOntoScreen(x.x, x.y);
+            }
+
             count++;
         });
+        // total up the area of each lowest point
         point.area = count;
     }
 
+    // sort by area size
     lowPoints.sort((a, b) => {
         return b.area - a.area;
     });
 
+    // reduce multiply together the top 3 areas
     const totalArea = lowPoints.slice(0, 3).reduce((acc, point) => {
         return acc * point.area;
     }, 1);
-    
+
     await Advent.Submit(totalArea, 2);
+
+    S.Draw();
 }
 Run();
