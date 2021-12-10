a6fe5083cc2a6c86441a6d426d21922447a0336b
Jamie Holding <me@jamie.holdings>
2021-12-10 06:08:43 +0000

    [+] Day 10 part 1

--- 10.js
+++ 10.js
@@ -3,8 +3,54 @@ const Advent = new AdventLib(10, 2021);
 
 async function Run() {
     const input = await Advent.GetInput();
+    const input2 = `[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]`.split(/\n/);
 
-    // await Advent.Submit(null);
+    const chars = {
+        '(': ')',
+        '[': ']',
+        '<': '>',
+        '{': '}',
+    };
+
+    const pairs = Object.keys(chars).map(key => [key, chars[key]].join(''));
+
+    // return undefined if all is fine, otherwise return invalid character
+    const validate = (line) => {
+        // remove valid pairs of symbols until we cannot find any more
+        while (pairs.find((pair) => {
+            const newLine = line.replace(pair, '');
+            if (newLine !== line) {
+                line = newLine;
+                return true;
+            }
+            return false;
+        }) !== undefined) {}
+        return line.split('').find((x) => {
+            return Object.values(chars).indexOf(x) >= 0;
+        });
+    };
+
+    const invalidChars = input.map(validate);
+    const points = {
+        ')': 3,
+        ']': 57,
+        '}': 1197,
+        '>': 25137,
+    };
+    const ans1 = invalidChars.reduce((acc, x) => {
+        return acc + (points[x] || 0);
+    }, 0);
+
+    await Advent.Submit(ans1);
     // await Advent.Submit(null, 2);
 }
 Run();
