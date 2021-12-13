9f3d19189347062ad04afda830b6c4dce98bdd65
Jamie Holding <me@jamie.holdings>
2021-12-13 20:10:04 +0000

    [~] Generate files for a given year

--- generate_files.js
+++ generate_files.js
@@ -5,8 +5,11 @@ import { promises as fs, existsSync as fsExists } from 'fs';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const year = 2021;
 
-async function generate_file(day) {
-    const entryFileName = pathJoin(__dirname, day.toString().padStart(2, '0') + '.js');
+async function generate_file(day, year = 2021) {
+    const entryFileName = pathJoin(__dirname, year.toString(), day.toString().padStart(2, '0') + '.js');
+    try {
+        await fs.mkdir(pathJoin(__dirname, year.toString()));
+    } catch(e) {}
     if (await fsExists(entryFileName)) return;
     await fs.writeFile(entryFileName, `import { Advent as AdventLib } from './lib/advent.js';
 const Advent = new AdventLib(${day}, ${year});
@@ -28,8 +31,11 @@ const run = async () => {
         await fs.mkdir(inputsDir);
     }
 
+    // get year from command line (or just use current year)
+    const year = Number(process.argv[2] || new Date().getFullYear());
+
     for (let i = 1; i <= 25; i++) {
-        generate_file(i);
+        generate_file(i, year);
     }
 };
 run();
