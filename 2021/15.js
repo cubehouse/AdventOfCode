f1a38d862bd660a39c43f1b473020003fd3d1ca3
Jamie Holding <me@jamie.holdings>
2021-12-15 07:24:16 +0000

    [+] Day 15 part 2

--- 2021/15.js
+++ 2021/15.js
@@ -27,7 +27,7 @@ async function Run() {
     W.applyASCIIMap(0, 0, input, colours);
     W.loop();
 
-    const findPath = async () => {
+    const findPath = async (maze) => {
         const visited = { '0,0': 0 };
         const dirs = [
             { x: 0, y: -1 },
@@ -35,8 +35,10 @@ async function Run() {
             { x: 0, y: 1 },
             { x: -1, y: 0 },
         ];
-        const target = { x: W.width - 1, y: W.height - 1 };
+        const target = { x: maze.width - 1, y: maze.height - 1 };
         const todo = [{ x: 0, y: 0, risk: 0, path: [] }];
+
+        let step = 0;
 
         while (todo.length > 0) {
             const next = todo.shift();
@@ -51,11 +53,11 @@ async function Run() {
             for (const dir of dirs) {
                 const nextX = next.x + dir.x;
                 const nextY = next.y + dir.y;
-                if (nextX < 0 || nextX >= W.width || nextY < 0 || nextY >= W.height) {
+                if (nextX < 0 || nextX >= maze.width || nextY < 0 || nextY >= maze.height) {
                     continue;
                 }
                 const nextKey = `${nextX},${nextY}`;
-                const newDirRisk = next.risk + W.getKey(nextX, nextY, 'risk');
+                const newDirRisk = next.risk + maze.getKey(nextX, nextY, 'risk');
                 if (visited[nextKey] && visited[nextKey] <= newDirRisk) {
                     continue;
                 }
@@ -72,10 +74,16 @@ async function Run() {
             }
 
             todo.sort((a, b) => a.risk - b.risk);
+
+            step++;
+            // yield occasionally so Windows doesn't think we've crashed
+            if (step % 10000 === 0) {
+                await new Promise((resolve) => setTimeout(resolve, 0));
+            }
         }
         return [];
     };
-    const path = await findPath();
+    const path = await findPath(W);
 
     for (let i = 0; i < path.length; i++) {
         const cell = path[i];
@@ -86,6 +94,47 @@ async function Run() {
     const ans1 = path[path.length - 1].risk;
     await Advent.Submit(ans1);
 
-    // await Advent.Submit(null, 2);
+    // W.stop();
+
+    // part 2
+    const W2 = new Window({
+        width: input[0].length * 5, height: input.length * 5,
+    });
+
+    for (let x = 0; x < input[0].length; x++) {
+        for (let y = 0; y < input.length; y++) {
+            const risk = input[y][x];
+
+            for (let xx = 0; xx < 5; xx++) {
+                for (let yy = 0; yy < 5; yy++) {
+                    const cell = {
+                        x: xx * input[0].length + x,
+                        y: yy * input.length + y,
+                    };
+                    let newRisk = (risk + xx + yy);
+                    while (newRisk > 9) {
+                        newRisk -= 9;
+                    }
+                    W2.setPixel(cell.x, cell.y, colours[newRisk]);
+                    W2.setKey(cell.x, cell.y, 'risk', newRisk);
+                }
+            }
+        }
+    }
+
+    W2.loop();
+
+    console.log('Solving maze...');
+    const path2 = await findPath(W2);
+    console.log('Solved! Drawing route...');
+    
+    const ans2 = path2[path2.length - 1].risk;
+    await Advent.Submit(ans2, 2);
+    
+    for (let i = 0; i < path2.length; i++) {
+        const cell = path2[i];
+        W2.setPixel(cell.x, cell.y, Window.colourLerp(Window.red, Window.green, i / path2.length));
+        await new Promise(resolve => setTimeout(resolve, 5));
+    }
 }
 Run();
