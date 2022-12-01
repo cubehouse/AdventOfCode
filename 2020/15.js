a5750a149471c7388e7a5ff39c3b1e632f5fe517
Jamie Holding <me@jamie.holdings>
2022-12-01 08:14:58 +0000

    2020 day 15

--- 2020/15.js
+++ 2020/15.js
@@ -5,9 +5,33 @@ const Advent = new AdventLib(15, 2020);
 // import Window from '../lib/window.js';
 
 async function Run() {
-    const input = await Advent.GetInput();
-
-    // await Advent.Submit(null);
-    // await Advent.Submit(null, 2);
+    const input = (await Advent.GetInput()).split(',').map(x => parseInt(x));
+    
+    for(let i = input.length; i < 2020; i++) {
+        let last = input[i-1];
+        let lastpos = input.lastIndexOf(last, i-2);
+        if(lastpos == -1) {
+            input.push(0);
+        } else {
+            input.push(i - lastpos - 1);
+        }
+    }
+    await Advent.Submit(input[2019]);
+    
+    // part 2
+    let lastpos = new Map();
+    for(let i = 0; i < input.length - 1; i++) {
+        lastpos.set(input[i], i);
+    }
+    let last = input[input.length - 1];
+    for(let i = input.length; i < 30000000; i++) {
+        let next = 0;
+        if(lastpos.has(last)) {
+            next = i - lastpos.get(last) - 1;
+        }
+        lastpos.set(last, i - 1);
+        last = next;
+    }
+    await Advent.Submit(last, 2);
 }
 Run();
