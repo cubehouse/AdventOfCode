1bfb4666009a395ed436c7900ad261bd1062b000
Jamie Holding <me@jamie.holdings>
2022-12-04 17:23:56 +0000

    [+] Day 4

--- 2022/04.js
+++ 2022/04.js
@@ -7,7 +7,37 @@ const Advent = new AdventLib(4, 2022);
 async function Run() {
     const input = await Advent.GetInput();
 
-    // await Advent.Submit(null);
-    // await Advent.Submit(null, 2);
+    const groups = input.map((x) => {
+        // regex parse in format \d-\d,\d-\d
+        const regex = /(\d+)-(\d+),(\d+)-(\d+)/;
+        const match = x.match(regex);
+        if (match === null) {
+            throw new Error("Invalid input");
+        }
+        const [, x1, x2, y1, y2] = match;
+
+        return {
+            x1: parseInt(x1),
+            x2: parseInt(x2),
+            y1: parseInt(y1),
+            y2: parseInt(y2),
+        };
+    });
+
+    const fullyOverlappingGroups = groups.filter((x) => {
+        return (x.x1 >= x.y1 && x.x2 <= x.y2) || (x.y1 >= x.x1 && x.y2 <= x.x2);
+    });
+
+    await Advent.Submit(fullyOverlappingGroups.length);
+
+    // part 2
+
+    const overlappingPairs = groups.filter((x) => {
+        // 1D line intersection
+        return (x.y1 >= x.x1 && x.y1 <= x.x2) || (x.y2 >= x.x1 && x.y2 <= x.x2)
+        || (x.x1 >= x.y1 && x.x1 <= x.y2) || (x.x2 >= x.y1 && x.x2 <= x.y2);
+    });
+
+    await Advent.Submit(overlappingPairs.length, 2);
 }
 Run();
