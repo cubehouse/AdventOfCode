6c2d20e56835d910c5b7447a7428cf9cfacddb6c
Jamie Holding <me@jamie.holdings>
2022-12-06 11:48:10 +0000

    [+] Day 6

--- 2022/06.js
+++ 2022/06.js
@@ -7,7 +7,26 @@ const Advent = new AdventLib(6, 2022);
 async function Run() {
     const input = await Advent.GetInput();
 
-    // await Advent.Submit(null);
-    // await Advent.Submit(null, 2);
+    // find first set of 4 characters that are unique
+    const findUnique = (str, size = 4) => {
+        let uniqueIndexes = [];
+        str.split('').forEach((c, i) => {
+            const chars = input.slice(i, i + size);
+            if (chars.length === size) {
+                // mush into a set, which skips duplicates
+                const unique = [...new Set(chars.split(''))].join('');
+                if (unique.length === size) {
+                    uniqueIndexes.push(i + size);
+                }
+            }
+        });
+        return uniqueIndexes;
+    };
+
+    await Advent.Submit(findUnique(input, 4)[0]);
+
+    // part 2
+    // do the same, but for 14 characters
+    await Advent.Submit(findUnique(input, 14)[0], 2);
 }
 Run();
