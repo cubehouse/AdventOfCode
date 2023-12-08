565f9a1316fac2882280ee51ecf780cfb52eb964
Jamie Holding <me@jamie.holdings>
2023-12-08 23:37:38 +0000

    [+] Day 8 Part 2

--- 2023/08.js
+++ 2023/08.js
@@ -23,10 +23,7 @@ class Node {
 const D_LEFT = 0;
 const D_RIGHT = 1;
 
-async function Run() {
-    const input = await Advent.GetInput();
-
-    // build our maze
+function BuildMaze(input) {
     const maze = {};
     const instructions = [];
     input.forEach((line, idx) => {
@@ -61,6 +58,18 @@ async function Run() {
         }
     });
 
+    return [
+        maze,
+        instructions,
+    ];
+}
+
+async function Run() {
+    let input = await Advent.GetInput();
+
+    // build our maze
+    const [maze, instructions] = BuildMaze(input);
+
     Advent.Assert(maze['AAA'].left.id, 'BBB');
     Advent.Assert(maze['AAA'].right.id, 'BBB');
     //Advent.Assert(maze['BBB'].left.id, 'AAA');
@@ -88,6 +97,62 @@ async function Run() {
     const ans1 = state.steps;
 
     await Advent.Submit(ans1);
-    // await Advent.Submit(null, 2);
+
+    // === Part 2 ===
+
+    if (Advent.SampleMode) {
+        // replace input
+        const sample2 = `LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`;
+        input = sample2.split('\n');
+    }
+
+    const [maze2, instructions2] = BuildMaze(input);
+
+    const state2 = Object.keys(maze2).filter((x) => {
+        // return keys that end with A
+        return x.match(/A$/);
+    }).map((x) => {
+        return {
+            start: maze2[x],
+            position: maze2[x],
+            steps: 0,
+        };
+    });
+
+    // find each path's cycle to their Z node
+    state2.forEach((state) => {
+        let idx = 0;
+        while(!state.position.id.match(/Z$/)) {
+            const instruction = instructions2[idx];
+            if (instruction === D_LEFT) {
+                state.position = state.position.left;
+            } else if (instruction === D_RIGHT) {
+                state.position = state.position.right;
+            } else {
+                Advent.Assert(false, 'Unknown instruction');
+            }
+            state.steps++;
+            idx = (idx + 1) % instructions2.length;
+        };
+    });
+
+    // thank you https://stackoverflow.com/questions/47047682/least-common-multiple-of-an-array-values-using-euclidean-algorithm
+    const gcd = (a, b) => a ? gcd(b % a, a) : b;
+    const lcm = (a, b) => a * b / gcd(a, b);
+
+    const ans2 = state2.reduce((acc, state) => {
+        return lcm(acc, state.steps);
+    }, 1);
+
+    await Advent.Submit(ans2, 2);
 }
 Run();
