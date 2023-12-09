5a999e69aab686e1577c2d136ffe6c048fc7a819
Jamie Holding <me@jamie.holdings>
2023-12-09 19:17:40 +0000

    [+] Day 9 Part 1

--- 2023/09.js
+++ 2023/09.js
@@ -1,13 +1,65 @@
 import { Advent as AdventLib } from '../lib/advent.js';
 const Advent = new AdventLib(9, 2023);
 
-// UI library
-// import Window from '../lib/window.js';
+class Sequence {
+    constructor(input) {
+        this.numbers = input.split(/\s/g).map(Number);
+
+        this.sequences = [[].concat(this.numbers)];
+
+        this.Evaluate();
+    }
+
+    static GenerateDiffArray(in_arr) {
+        const arr = in_arr.slice();
+        const diffs = [];
+        for (let i = 0; i < arr.length - 1; i++) {
+            diffs.push(arr[i + 1] - arr[i]);
+        }
+        return diffs;
+    }
+
+    static IsArrayAllZeroes(arr) {
+        return arr.every((x) => x === 0);
+    }
+
+    Evaluate() {
+        let sequenceDepth = 0;
+        let currentSequence = [].concat(this.numbers);
+        
+        while (!Sequence.IsArrayAllZeroes(currentSequence)) {
+            currentSequence = Sequence.GenerateDiffArray(currentSequence);
+            this.sequences.push(currentSequence);
+            sequenceDepth++;
+        }
+
+        this.depth = sequenceDepth;
+        return sequenceDepth;
+    }
+
+    GenerateNextValue() {
+        return this.sequences.reduce((acc, sequence) => {
+            return acc + sequence[ sequence.length - 1 ];
+        }, 0);
+    }
+}
 
 async function Run() {
     const input = await Advent.GetInput();
 
-    // await Advent.Submit(null);
+    const sequences = input.map((line) => new Sequence(line));
+    Advent.Assert(sequences[0].GenerateNextValue(), 18);
+    Advent.Assert(sequences[1].GenerateNextValue(), 28);
+    Advent.Assert(sequences[2].GenerateNextValue(), 68);
+
+    const ans1 = sequences.reduce((acc, sequence) => {
+        const nextNum = sequence.GenerateNextValue();
+        return acc + nextNum;
+    }, 0);
+
+    await Advent.Submit(ans1);
+
+    
     // await Advent.Submit(null, 2);
 }
 Run();
