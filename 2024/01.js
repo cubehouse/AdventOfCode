b556caff07d96e55189946eed5cd9b4f9f820860
Jamie Holding <me@jamie.holdings>
2024-12-01 10:25:26 +0000

    [+] 2024 Day 1 Part 1

--- 2024/01.js
+++ 2024/01.js
@@ -7,7 +7,27 @@ const Advent = new AdventLib(1, 2024);
 async function Run() {
     const input = await Advent.GetInput();
 
-    // await Advent.Submit(null);
+    const leftList = [];
+    const rightList = [];
+
+    input.forEach((line) => {
+        const [left, right] = line.split(/\s+/);
+        leftList.push(parseInt(left));
+        rightList.push(parseInt(right));
+    });
+
+    leftList.sort();
+    rightList.sort();
+
+    const pairs = leftList.map((x, idx) => {
+        return [x, rightList[idx]];
+    });
+
+    const ans1 = pairs.reduce((acc, [left, right]) => {
+        return acc + Math.abs(left - right);
+    }, 0);
+
+    await Advent.Submit(ans1);
     // await Advent.Submit(null, 2);
 }
 Run();
