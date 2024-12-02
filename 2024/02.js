a575cb3fe0f1de8b08443b3ccc2a21752c2730b4
Jamie Holding <me@jamie.holdings>
2024-12-02 10:50:32 +0000

    [+] 2024 Day 2 Part 2

--- 2024/02.js
+++ 2024/02.js
@@ -12,30 +12,37 @@ async function Run() {
 8 6 4 4 1
 1 3 6 7 9`);
 
-    const reports = input.map((x) => x.split(/\s+/).map(Number)).map((x) => {
-        const levelsIncreasing = x.every((y, i, arr) => i == 0 || y >= arr[i - 1]);
-        const levelsDecreasing = x.every((y, i, arr) => i == 0 || y <= arr[i - 1]);
+    const isReportValid = (report) => {
+        const levelsIncreasing = report.every((y, i, arr) => i == 0 || y >= arr[i - 1]);
+        const levelsDecreasing = report.every((y, i, arr) => i == 0 || y <= arr[i - 1]);
 
-        const safeDifferences = x.every((b, i, arr) => {
+        const safeDifferences = report.every((b, i, arr) => {
             if (i == 0) return true;
             const a = arr[i - 1];
             const diff = Math.abs(a - b);
             return diff >= 1 && diff <= 3;
         });
+        
+        return (levelsIncreasing || levelsDecreasing) && safeDifferences;
+    };
+    const reports = input.map((x) => x.split(/\s+/).map(Number));
 
-        return {
-            levelsIncreasing,
-            levelsDecreasing,
-            safeDifferences,
-        };
-    });
-
-    const ans1 = reports.filter((x) => {
-        return (x.levelsIncreasing || x.levelsDecreasing) && x.safeDifferences;
-    }).length;
-
+    const ans1 = reports.filter(isReportValid).length;
     await Advent.Submit(ans1);
-    
-    // await Advent.Submit(null, 2);
+
+    const ans2 = reports.filter((x) => {
+        const safe = isReportValid(x);
+        if (safe) return true;
+
+        // try fixing report by removing each entry and re-testing
+        //  we can tolerate one invalid entry
+        for (let i = 0; i < x.length; i++) {
+            const copy = [...x];
+            copy.splice(i, 1);
+            if (isReportValid(copy)) return true;
+        }
+        return false;
+    }).length;
+    await Advent.Submit(ans2, 2);
 }
 Run();
