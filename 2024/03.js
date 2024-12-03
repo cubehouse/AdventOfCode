c55667459f6fd0c8e15db219211a928b125e3c94
Jamie Holding <me@jamie.holdings>
2024-12-03 12:11:53 +0000

    [+] 2024 Day 3 Part 2

--- 2024/03.js
+++ 2024/03.js
@@ -5,7 +5,8 @@ const Advent = new AdventLib(3, 2024);
 // import Window from '../lib/window.js';
 
 async function Run() {
-    const input = await Advent.GetInput(`xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+    const input = await Advent.GetInput(`
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))mul(1,1)mul(8,5)don't()dsado()don't()mul(1,4)
 `);
 
     const mulRegex = /mul\(([0-9]+),([0-9]+)\)/g;
@@ -23,6 +24,18 @@ async function Run() {
     }, 0);
 
     await Advent.Submit(ans1);
-    // await Advent.Submit(null, 2);
+
+    // I *could* write a clever regex, or just strip out things between don't() and do()
+    let ans2 = 0;
+    const lineStripped = `do()${input.join("")}do()`.replace(/don't\(\).*?(?:do\(\)|$)/g, '');
+    let match;
+    while ((match = mulRegex.exec(lineStripped)) !== null) {
+        const A = parseInt(match[1]);
+        const B = parseInt(match[2]);
+
+        ans2 += A * B;
+    }
+
+    await Advent.Submit(ans2, 2);
 }
 Run();
