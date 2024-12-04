c49b4af84d936d0f2672fbb6dd8334922392b7a5
Jamie Holding <me@jamie.holdings>
2024-12-04 11:49:47 +0000

    [+] 2024 Day 4 Part 2

--- 2024/04.js
+++ 2024/04.js
@@ -5,16 +5,16 @@ import Screen from '../lib/screen.js';
 
 
 async function Run() {
-    const input = await Advent.GetInput(`MMMSXXMASM
-MSAMXMSMSA
-AMXSXMAAMM
-MSAMASMSMX
-XMASAMXAMM
-XXAMMXXAMA
-SMSMSASXSS
-SAXAMASAAA
-MAMMMXMMMM
-MXMXAXMASX`);
+    const input = await Advent.GetInput(`.M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+..........`);
 
     const S = new Screen();
 
@@ -73,7 +73,8 @@ MXMXAXMASX`);
             }
         }
 
-        await new Promise((resolve) => setTimeout(resolve, 0));
+        // animation
+        //await new Promise((resolve) => setTimeout(resolve, 0));
 
         return wordsFound;
     };
@@ -86,6 +87,74 @@ MXMXAXMASX`);
     }
 
     await Advent.Submit(totalWordsFound);
-    // await Advent.Submit(null, 2);
+
+    // reset style
+    S.ForEachCell((cell) => {
+        cell.style = undefined;
+    });
+
+
+    // 4 possible diagonals and their partners
+    const diagonals = [
+        [
+            [-1, -1],
+            [1, 1]
+        ],
+        [
+            [1, -1],
+            [-1, 1]
+        ],
+        [
+            [1, 1],
+            [-1, -1]
+        ],
+        [
+            [-1, 1],
+            [1, -1]
+        ],
+    ];
+    const searchXMas = (x, y) => {
+        const center = S.Get(x, y);
+        if (center !== 'A') {
+            return false;
+        }
+
+        const masCount = diagonals.reduce((acc, diagonal) => {
+            const diag1 = S.Get(x + diagonal[0][0], y + diagonal[0][1]);
+            const diag2 = S.Get(x + diagonal[1][0], y + diagonal[1][1]);
+
+            if (diag1 === 'M' && diag2 === 'S') {
+                acc++;
+                S.SetKey(x, y, 'style', '{green-fg}');
+                S.SetKey(x + diagonal[0][0], y + diagonal[0][1], 'style', '{green-fg}');
+                S.SetKey(x + diagonal[1][0], y + diagonal[1][1], 'style', '{green-fg}');
+            }
+
+            return acc;
+        }, 0);
+
+        if (masCount === 2) {
+            return true;
+        }
+        return false;
+    };
+
+    let xMasCount = 0;
+    for (let y = 0; y < S.height; y++) {
+        for (let x = 0; x < S.width; x++) {
+            if (searchXMas(x, y)) {
+                xMasCount++;
+            }
+        }
+    }
+
+    await Advent.Submit(xMasCount, 2);
+
+    // colour unused cell in red because Christmas
+    S.ForEachCell((cell) => {
+        if (cell.style === undefined) {
+            cell.style = '{red-fg}';
+        }
+    });
 }
 Run();
