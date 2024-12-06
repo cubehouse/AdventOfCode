e64b57ae55847f492d260580d123b8c4b19cf6fa
Jamie Holding <me@jamie.holdings>
2024-12-06 09:20:08 +0000

    [+] 2024 Day 6 Part 2

--- 2024/06.js
+++ 2024/06.js
@@ -52,6 +52,7 @@ async function Run() {
             S.Set(x, y, char);
         });
     });
+    const guardPosOriginal = { ...guardPos };
 
     const guardStep = async () => {
         // trace in direction
@@ -65,7 +66,8 @@ async function Run() {
             return cell?.val !== '#';
         });
 
-        for(const { x, y } of tracePath) {
+        for (const { x, y } of tracePath) {
+            S.SetKey(x, y, 'visitedP1', true);
             S.SetKey(x, y, 'visited', true);
             S.SetKey(x, y, 'style', '{red-fg}');
             await anim();
@@ -82,7 +84,7 @@ async function Run() {
         return false;
     };
 
-    while(true) {
+    while (true) {
         const leftArea = await guardStep();
         if (leftArea) {
             break;
@@ -97,8 +99,111 @@ async function Run() {
     }, 0);
 
     Advent.Assert(ans1, 41);
-
     await Advent.Submit(ans1);
-    // await Advent.Submit(null, 2);
+
+    const clearMaze = () => {
+        // clear visited state
+        S.ForEachCell((cell) => {
+            cell.visited = undefined;
+            S.SetKey(cell.x, cell.y, 'style', '{white-fg}');
+        });
+    };
+
+    // attempt to cause a loop by placing on this cell
+    const attempt = async (x, y) => {
+        console.log(`Attempting ${x}, ${y}`);
+
+        // if cell is already a wall, skip
+        if (S.Get(x, y) === '#') {
+            return false;
+        }
+
+        // if player never travelled here in P1, skip
+        //  this is a fairly major optimisation in a terrible solution
+        if (!S.GetKey(x, y, 'visitedP1')) {
+            return false;
+        }
+
+        guardDir = 0;
+        guardPos = { ...guardPosOriginal };
+
+        // can't place wall on guard
+        if (x === guardPos.x && y === guardPos.y) {
+            return false;
+        }
+
+        S.Set(x, y, '#');
+
+        clearMaze();
+
+        let loop = false;
+
+        const guardStepLookForLoop = async () => {
+            // trace in direction
+            const dir = directions[guardDir];
+            let hitEdge = false;
+            const tracePath = S.Trace(guardPos.x, guardPos.y, dir[0], dir[1], (cell) => {
+                if (!cell) {
+                    hitEdge = true;
+                    return false;
+                }
+                return cell?.val !== '#';
+            });
+
+            for (const { x, y } of tracePath) {
+                const visitedArr = S.GetKey(x, y, 'visited');
+                if (visitedArr) {
+                    // check if we have a loop
+                    if (visitedArr.includes(dir)) {
+                        loop = true;
+                        return true;
+                    } else {
+                        S.SetKey(x, y, 'visited', [...visitedArr, dir]);
+                    }
+                } else {
+                    S.SetKey(x, y, 'visited', [dir]);
+                }
+                S.SetKey(x, y, 'style', '{red-fg}');
+            }
+
+            // rotate
+            guardDir = (guardDir + 1) % directions.length;
+            guardPos = tracePath[tracePath.length - 1];
+
+            // return true if we left the area
+            if (hitEdge) {
+                return true;
+            }
+            return false;
+        };
+
+        while (!loop) {
+            const leftArea = await guardStepLookForLoop();
+            if (leftArea) {
+                break;
+            }
+        }
+
+        await anim();
+
+        // clear wall
+        S.Set(x, y, '.');
+
+        if (loop) {
+            return true;
+        }
+    };
+
+    let loops = 0;
+    for(let y = 0; y <= S.maxY; y++) {
+        for(let x = 0; x <= S.maxX; x++) {
+            if (await attempt(x, y)) {
+                loops++;
+            }
+        }
+    }
+
+    Advent.Assert(loops, 6);
+    await Advent.Submit(loops, 2);
 }
 Run();
