7b693e83538e0af89a55a505d2bd2a7cbde38972
Jamie Holding <me@jamie.holdings>
2024-12-08 09:16:07 +0000

    [+] 2024 Day 8 Part 2

--- 2024/08.js
+++ 2024/08.js
@@ -96,6 +96,57 @@ async function Run() {
     Advent.Assert(ans1, 14);
     await Advent.Submit(ans1);
 
-    // await Advent.Submit(null, 2);
+    const traceAntennaPairToExtents = (a, b) => {
+        const delta = { x: b.x - a.x, y: b.y - a.y };
+
+        const points = [a, b];
+
+        const tracePath = (start, delta) => {
+            let x = start.x;
+            let y = start.y;
+            while(x >= 0 && y >= 0 && x < input[0].length && y < input.length) {
+                if (x < 0 || y < 0 || x >= input[0].length || y >= input.length) {
+                    break;
+                }
+                points.push({ x, y, id: start.id });
+                x += delta.x;
+                y += delta.y;
+            }
+        };
+        tracePath(a, delta);
+        tracePath(a, { x: -delta.x, y: -delta.y });
+
+        return points;
+    };
+
+    // for each antenna group, trace a line between each pair of antennas
+    for(const group of antennaGroups) {
+        for(let i = 0; i < group.antennas.length; i++) {
+            for(let j = i + 1; j < group.antennas.length; j++) {
+                const [a, b] = [group.antennas[i], group.antennas[j]];
+                const points = traceAntennaPairToExtents(a, b);
+                for(const point of points) {
+                    if (point.x < 0 || point.y < 0 || point.x >= input[0].length || point.y >= input.length) {
+                        continue;
+                    }
+
+                    S.Set(point.x, point.y, '#');
+                    const antinodes = S.GetKey(point.x, point.y, 'antinode') || [];
+                    antinodes.push(point.id);
+                    S.SetKey(point.x, point.y, 'antinode', antinodes);
+                }
+                await new Promise((resolve) => setTimeout(resolve, 0));
+            }
+        }
+    }
+    const ans2 = S.Cells.reduce((acc, cell) => {
+        if (cell?.antinode) {
+            return acc + 1;
+        }
+        return acc;
+    }, 0);
+    Advent.Assert(ans2, 34);
+
+    await Advent.Submit(ans2, 2);
 }
 Run();
