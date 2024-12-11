81a58f137a55f87d7d00d1de86c71e75b62cf318
Jamie Holding <me@jamie.holdings>
2024-12-11 13:13:08 +0000

    [+] 2024 Day 11 Part 2

--- 2024/11.js
+++ 2024/11.js
@@ -1,49 +1,47 @@
 import { Advent as AdventLib } from '../lib/advent.js';
 const Advent = new AdventLib(11, 2024);
 
-// UI library
-// import Window from '../lib/window.js';
+const cache = {};
 
 async function Run() {
     const input = await Advent.GetInput(`125 17`);
 
     const stones = input.split(' ').map(Number);
 
-    const blink = () => {
-        for (let idx = 0; idx < stones.length; idx++) {
-            const stone = stones[idx];
-            if (stone === 0) {
-                stones[idx] = 1;
-                continue;
-            }
-
-            const stoneStr = `${stone}`;
-            if (stoneStr.length % 2 == 0) {
-                const lhs = stoneStr.slice(0, stoneStr.length / 2);
-                const rhs = stoneStr.slice(stoneStr.length / 2);
-                stones[idx] = Number(lhs);
-                stones.splice(idx + 1, 0, Number(rhs));
-                idx += 1;
-                continue;
-            }
-
-            stones[idx] = stones[idx] * 2024;
+    // expand stone value, returning how many stones are in the expanded state
+    const _expandStone = (stone, depth) => {
+        if (stone == 0) {
+            return expandStone(1, depth - 1);
         }
-    };
-
-    for (let i = 0; i < 25; i++) {
-        blink();
+        const stoneStr = `${stone}`;
+        if (stoneStr.length % 2 == 0) {
+            const lhs = stoneStr.slice(0, stoneStr.length / 2);
+            const rhs = stoneStr.slice(stoneStr.length / 2);
+            return expandStone(Number(lhs), depth - 1) + expandStone(Number(rhs), depth - 1);
+        } else {
+            return expandStone(stone * 2024, depth - 1);
+        }
     }
-    const ans1 = stones.length;
-    Advent.Assert(ans1, 55312);
+    const expandStone = (stone, depth) => {
+        if (depth == 0) {
+            return 1;
+        }
 
+        const key = `${stone}_${depth}`;
+        if (cache[key]) {
+            return cache[key];
+        }
+        const size = _expandStone(stone, depth);
+        cache[key] = size;
+        return size;
+    }
+
+    const ans1 = stones.reduce((acc, stone) => acc + expandStone(stone, 25), 0);
+    Advent.Assert(ans1, 55312);
     await Advent.Submit(ans1);
 
-    // for (let i = 0; i < 50; i++) {
-    //     blink();
-    // }
-    // const ans2 = stones.length;
-    // await Advent.Submit(ans2, 2);
-    
+    const ans2 = stones.reduce((acc, stone) => acc + expandStone(stone, 75), 0);
+    await Advent.Submit(ans2, 2);
+
 }
 Run();
