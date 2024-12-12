88a1d8fa24aa05a44920ad3a245793148ee40435
Jamie Holding <me@jamie.holdings>
2024-12-12 14:25:12 +0000

    [+] 2024 Day 12 Part 2

--- 2024/12.js
+++ 2024/12.js
@@ -29,6 +29,7 @@ MMMISSJEEE`);
     input.forEach((line, y) => {
         line.split('').forEach((val, x) => {
             S.Set(x, y, val);
+            S.SetKey(x, y, 'crop', val);
 
             // pick colour from ASCII value
             const col = Colour.basicColours[(val.charCodeAt(0) - 65) % Colour.basicColours.length];
@@ -83,7 +84,7 @@ MMMISSJEEE`);
                 // callback function when we find a cell
                 region.cells.push(cell);
                 S.SetKey(cell.x, cell.y, 'region', region),
-                region.fenceCount += S.GetKey(cell.x, cell.y, 'fenceCount') || 0;
+                    region.fenceCount += S.GetKey(cell.x, cell.y, 'fenceCount') || 0;
                 //S.SetStyle(cell.x, cell.y, `{green-bg}`);
             });
         }
@@ -93,8 +94,91 @@ MMMISSJEEE`);
         return acc + (region.fenceCount * region.cells.length);
     }, 0);
     Advent.Assert(ans1, 1930);
-
     await Advent.Submit(ans1);
-    // await Advent.Submit(null, 2);
+
+    const allDirs = [
+        [0, -1],
+        [1, 0],
+        [0, 1],
+        [-1, 0],
+        [1, -1],
+        [1, 1],
+        [-1, 1],
+        [-1, -1]
+    ];
+
+    const countCornersOfRegion = (region) => {
+        const corners = region.cells.reduce((acc, cell) => {
+            // check if cell is a corner
+            const n = allDirs.map((dir) => {
+                const [dx, dy] = dir;
+                return S.GetKey(cell.x + dx, cell.y + dy, 'crop') === cell.crop;
+            });
+
+            let cellCorners = 0;
+
+            // inner corners
+            if (n[0] && n[1] && !n[4]) {
+                cellCorners++;
+            }
+            if (n[1] && n[2] && !n[5]) {
+                cellCorners++;
+            }
+            if (n[2] && n[3] && !n[6]) {
+                cellCorners++;
+            }
+            if (n[3] && n[0] && !n[7]) {
+                cellCorners++;
+            }
+
+            // outer corners
+            if (n[0] && n[1] && !n[2] && !n[3]) {
+                cellCorners++;
+            }
+            if (n[1] && n[2] && !n[3] && !n[0]) {
+                cellCorners++;
+            }
+            if (n[2] && n[3] && !n[0] && !n[1]) {
+                cellCorners++;
+            }
+            if (n[3] && n[0] && !n[1] && !n[2]) {
+                cellCorners++;
+            }
+
+            // thin edge corners
+            if (n[0] && !n[1] && !n[2] && !n[3]) {
+                cellCorners += 2;
+            }
+            if (n[1] && !n[2] && !n[3] && !n[0]) {
+                cellCorners += 2;
+            }
+            if (n[2] && !n[3] && !n[0] && !n[1]) {
+                cellCorners += 2;
+            }
+            if (n[3] && !n[0] && !n[1] && !n[2]) {
+                cellCorners += 2;
+            }
+
+            if (!n[0] && !n[1] && !n[2] && !n[3]) {
+                // single cell
+                cellCorners = 4;
+            }
+            
+            S.Set(cell.x, cell.y, `${cellCorners}`);
+
+            return acc + cellCorners;
+        }, 0);
+
+        return corners;
+    };
+    regions.forEach((region) => {
+        region.edges = countCornersOfRegion(region);
+    });
+
+    const ans2 = regions.reduce((acc, region) => {
+        return acc + (region.edges * region.cells.length);
+    }, 0);
+    Advent.Assert(ans2, 1206);
+    await Advent.Submit(ans2, 2);
 }
 Run();
