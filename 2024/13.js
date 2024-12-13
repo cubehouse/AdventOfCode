40ea00fcbaf03954d59033828013f7c0baef9ecd
Jamie Holding <me@jamie.holdings>
2024-12-13 05:26:20 +0000

    [+] 2024 Day 13 Part 1. Yeh I thought part 2 would do that

--- 2024/13.js
+++ 2024/13.js
@@ -5,9 +5,70 @@ const Advent = new AdventLib(13, 2024);
 // import Window from '../lib/window.js';
 
 async function Run() {
-    const input = await Advent.GetInput();
+    const input = await Advent.GetInputRaw(`Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
 
-    // await Advent.Submit(null);
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279`);
+
+    const machines = [];
+    const machineRegex = /Button A: X((?:\+|\-)\d+), Y((?:\+|\-)\d+)\nButton B: X((?:\+|\-)\d+), Y((?:\+|\-)\d+)\nPrize: X=(\d+), Y=(\d+)/g;
+
+    let match;
+    while (match = machineRegex.exec(input)) {
+        machines.push({
+            a: { x: parseInt(match[1]), y: parseInt(match[2]) },
+            b: { x: parseInt(match[3]), y: parseInt(match[4]) },
+            prize: { x: parseInt(match[5]), y: parseInt(match[6]) },
+        });
+    }
+
+    // cba.
+    const bruteForceMachine = (machine) => {
+        const validCombos = [];
+        for (let x = 0; x < 100; x++) {
+            for (let y = 0; y < 100; y++) {
+                const pos = [
+                    machine.a.x * x + machine.b.x * y,
+                    machine.a.y * x + machine.b.y * y,
+                ];
+
+                if (pos[0] === machine.prize.x && pos[1] === machine.prize.y) {
+                    validCombos.push({
+                        x,
+                        y,
+                        cost: (x * 3) + y,
+                    });
+                }
+            }
+        }
+        validCombos.sort((a, b) => a.cost - b.cost);
+
+        return validCombos;
+    };
+
+    const validCombos = machines.map(bruteForceMachine);
+
+    const ans1 = validCombos.reduce((acc, x) => {
+        if (x.length === 0) {
+            return acc;
+        }
+        return acc + x[0].cost;
+    }, 0);
+    Advent.Assert(ans1, 480);
+    await Advent.Submit(ans1);
+
+    
     // await Advent.Submit(null, 2);
 }
 Run();
