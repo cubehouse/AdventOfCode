dfbbb0a59dbeadab8706478ee1dcdb5cd3dbcdf6
Jamie Holding <me@jamie.holdings>
2024-12-18 14:30:53 +0000

    [+] 2024 Day 15 Part 2

--- 2024/15.js
+++ 2024/15.js
@@ -48,7 +48,7 @@ async function Run() {
             W.setPixel(x, y, cell === '#' ? Window.black : Window.white);
 
             if (cell === 'O') {
-                boxes.push({ x, y });
+                boxes.push({ x, y, width: 1, height: 1 });
             }
             if (cell === '@') {
                 robot.x = x;
@@ -57,60 +57,197 @@ async function Run() {
         });
     });
 
-    const tryMove = (x, y, dx, dy) => {
-        if (W.get(x + dx, y + dy).wall) {
-            return false;
-        }
-        for (const box of boxes) {
-            if (box.x === x + dx && box.y === y + dy) {
-                if (!tryMove(box.x, box.y, dx, dy)) {
-                    return false;
-                }
-                box.x += dx;
-                box.y += dy;
+    const isPointWithinBox = (point, box) => {
+        return point.x >= box.x && point.x < box.x + box.width &&
+            point.y >= box.y && point.y < box.y + box.height;
+    }
+
+    const getBoxCoordinates = (box) => {
+        const coords = [];
+        for (let x = box.x; x < box.x + box.width; x++) {
+            for (let y = box.y; y < box.y + box.height; y++) {
+                coords.push({ x, y });
             }
         }
+        return coords;
+    };
+
+    const testBoxMove = (box, delta) => {
+        // test if box can move into the new position
+        // calculate spaces to test based on box width and height
+        const boxSpaces = getBoxCoordinates(box);
+        const spacesToTest = boxSpaces.map((point) => {
+            return { x: point.x + delta.x, y: point.y + delta.y };
+        }).filter((point) => {
+            // remove spaces that are still within the box
+            return !isPointWithinBox(point, box);
+        });
+
+        // test each space for a wall
+        const hitWall = spacesToTest.find((point) => {
+            return !!W.getKey(point.x, point.y, 'wall');
+        });
+
+        // if we hit a wall, return empty array (no things moved)
+        if (hitWall) {
+            return [];
+        }
+
+        // otherwise, look for any boxes in the way
+        const hitBoxes = boxes.filter((otherBox) => {
+            return spacesToTest.some((point) => {
+                return isPointWithinBox(point, otherBox);
+            });
+        });
+
+        // if we hit no boxes, return ourselves as a moved box
+        if (hitBoxes.length === 0) {
+            return [box];
+        }
+
+        // otherwise, recursively test the boxes
+        const hitBoxesMoves = [box];
+        for (const otherBox of hitBoxes) {
+            const moves = testBoxMove(otherBox, delta);
+            // if any box can't move, return empty array (no things moved)
+            if (moves.length == 0) {
+                return [];
+            }
+            hitBoxesMoves.push(...moves);
+        }
+
+        // if all boxes can move, return all boxes
+        return hitBoxesMoves;
+    }
+
+    const moveRobot = (delta) => {
+        const newRobot = { x: robot.x + delta.x, y: robot.y + delta.y };
+
+        // look for walls in the way
+        const hitWall = W.getKey(newRobot.x, newRobot.y, 'wall');
+        if (hitWall) {
+            return false;
+        }
+
+        // look for boxes in the way
+        const hitBox = boxes.find((box) => {
+            return isPointWithinBox(newRobot, box);
+        });
+
+        if (hitBox) {
+            const newBoxes = testBoxMove(hitBox, delta);
+            if (newBoxes.length === 0) {
+                return false;
+            }
+            // success! move all boxes
+            newBoxes.filter((box, idx, arr) => {
+                // remove duplicates
+                return arr.indexOf(box) === idx;
+            }).forEach((box) => {
+                for(const point of getBoxCoordinates(box)) {
+                    W.setPixel(point.x, point.y, Window.white);
+                }
+                box.x += delta.x;
+                box.y += delta.y;
+                for(const point of getBoxCoordinates(box)) {
+                    W.setPixel(point.x, point.y, Window.orange);
+                }
+            });
+        }
+
+        // made it this far, move the robot too
+        W.setPixel(robot.x, robot.y, Window.white);
+        robot.x = newRobot.x;
+        robot.y = newRobot.y;
+        W.setPixel(robot.x, robot.y, Window.green);
         return true;
     };
 
     const redraw = () => {
-        W.forEach((x, y, cell) => {
-            if (cell.wall) {
+        W.forEach((x, y) => {
+            const wall = W.getKey(x, y, 'wall');
+            if (wall) {
                 W.setPixel(x, y, Window.black);
             } else {
                 W.setPixel(x, y, Window.white);
             }
         });
 
-        W.setPixel(robot.x, robot.y, Window.green);
-        for (const box of boxes) {
-            W.setPixel(box.x, box.y, Window.blue);
-        }
+        boxes.forEach((box) => {
+            getBoxCoordinates(box).forEach((point) => {
+                W.setPixel(point.x, point.y, Window.orange);
+            });
+        });
     };
+
     redraw();
 
-    const tick = async () => {
-        let counter = 0;
-        for (const move of moves) {
-            if (tryMove(robot.x, robot.y, move.x, move.y)) {
-                robot.x += move.x;
-                robot.y += move.y;
-            }
-            counter++;
-            if (counter % 100 === 0) {
-                redraw();
-                await W.sleep(100);
-            }
+    let counter = 0;
+    for (const move of moves) {
+        moveRobot(move);
+        counter++;
+        if (counter % 10 === 0) {
+            await new Promise((resolve) => setTimeout(resolve, 0));
         }
-    };
-    await tick();
-    redraw();
+    }
 
     const ans1 = boxes.reduce((acc, box) => {
         return acc + (box.x + (box.y * 100));
     }, 0);
-    Advent.Assert(ans1, 10092);
     await Advent.Submit(ans1);
-    // await Advent.Submit(null, 2);
+
+    // Part 2
+    // clear the board
+    W.forEach((x, y) => {
+        W.setKey(x, y, 'wall', undefined);
+        W.setPixel(x, y, Window.white);
+    });
+    boxes.length = 0;
+
+    // rebuild map with new boxes
+    parsingMoves = false;
+    input.forEach((line, y) => {
+        if (line == '' || parsingMoves) {
+            parsingMoves = true;
+            return;
+        }
+
+        line.split('').forEach((cell, x) => {
+            if (cell === '#') {
+                W.setKey(x * 2, y, 'wall', true);
+                W.setKey((x * 2) + 1, y, 'wall', true);
+                W.setPixel(x * 2, y, Window.black);
+                W.setPixel((x * 2) + 1, y, Window.black);
+            } else if (cell === 'O') {
+                boxes.push({ x: x * 2, y, width: 2, height: 1 });
+                W.setKey(x * 2, y, 'wall', false);
+                W.setKey((x * 2) + 1, y, 'wall', false);
+            } else if (cell === '@') {
+                robot.x = x * 2;
+                robot.y = y;
+                W.setKey(x * 2, y, 'wall', false);
+                W.setKey((x * 2) + 1, y, 'wall', false);
+            } else {
+                W.setKey(x * 2, y, 'wall', false);
+                W.setKey((x * 2) + 1, y, 'wall', false);
+            }
+        });
+    });
+
+    redraw();
+    counter = 0;
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    for (const move of moves) {
+        moveRobot(move);
+        counter++;
+        if (counter % 10 === 0) {
+            await new Promise((resolve) => setTimeout(resolve, 0));
+        }
+    }
+
+    const ans2 = boxes.reduce((acc, box) => {
+        return acc + (box.x + (box.y * 100));
+    }, 0);
+    await Advent.Submit(ans2, 2);
 }
 Run();
