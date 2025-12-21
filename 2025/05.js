892444eba603bf368a592a65b6a7fde748ea2e88
Jamie Holding <me@jamie.holdings>
2025-12-21 20:49:01 +0000

    [+] 2025 Day 5 Part 1

--- 2025/05.js
+++ 2025/05.js
@@ -7,7 +7,28 @@ const Advent = new AdventLib(5, 2025);
 async function Run() {
     const input = await Advent.GetInput();
 
-    // await Advent.Submit(null);
+    const sepIndex = input.findIndex(line => line.trim() === '');
+
+    const freshRanges = input.slice(0, sepIndex).map(line => {
+        const [start, end] = line.split('-').map(Number);
+        return { start, end };
+    });
+    const ingredients = input.slice(sepIndex + 1).map(Number);
+
+    console.log('Input:', freshRanges, ingredients);
+
+    const isInAnyRange = (num) => {
+        return freshRanges.some(({ start, end }) => num >= start && num <= end);
+    };
+
+    let count = 0;
+    for (const ingredient of ingredients) {
+        if (isInAnyRange(ingredient)) {
+            count++;
+        }
+    }
+
+    await Advent.Submit(count);
     // await Advent.Submit(null, 2);
 }
 Run();
