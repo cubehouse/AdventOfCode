854ddd2f0a9f1f3a275da83c212c377823ba53f3
Jamie Holding <me@jamie.holdings>
2025-12-21 17:02:30 +0000

    [+] 2025 Day 3 Part 1

--- 2025/03.js
+++ 2025/03.js
@@ -7,6 +7,48 @@ const Advent = new AdventLib(3, 2025);
 async function Run() {
     const input = await Advent.GetInput();
 
+    const banks = input.map((x) => {
+        return x.split('').map(Number);
+    });
+
+    const findMaxIndexInArray = (arr) => {
+        let maxVal = -Infinity;
+        let maxIdx = -1;
+        arr.forEach((val, idx) => {
+            if (val > maxVal) {
+                maxVal = val;
+                maxIdx = idx;
+            }
+        });
+        return maxIdx;
+    };
+
+    const bankMaxVal = (bank) => {
+        // find max value
+        let digitOne = findMaxIndexInArray(bank);
+        // find second max value following digitOne
+        //  edge-case, if digitOne is at end of array, find the max in the front part and swap the digits around
+        let digitTwo = -1;
+        if (digitOne === bank.length - 1) {
+            const frontPart = bank.slice(0, digitOne);
+            digitTwo = bank.length - 1;
+            digitOne = findMaxIndexInArray(frontPart);
+        } else {
+            const backPart = bank.slice(digitOne + 1);
+            digitTwo = findMaxIndexInArray(backPart) + digitOne + 1;
+        }
+        return (bank[digitOne] * 10) + bank[digitTwo];
+    };
+
+    // Part 1
+    let part1Sum = 0;
+    banks.forEach((bank) => {
+        part1Sum += bankMaxVal(bank);
+    });
+
+    console.log('Part 1:', part1Sum);
+    await Advent.Submit(part1Sum, 1);
+
     // await Advent.Submit(null);
     // await Advent.Submit(null, 2);
 }
