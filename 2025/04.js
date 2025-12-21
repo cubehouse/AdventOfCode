20e6fba63f401bf16f72de676d72eb559f836f38
Jamie Holding <me@jamie.holdings>
2025-12-21 15:11:25 +0000

    [+] Empty 2025 files

---
+++ 2025/04.js
@@ -0,0 +1,13 @@
+import { Advent as AdventLib } from '../lib/advent.js';
+const Advent = new AdventLib(4, 2025);
+
+// UI library
+// import Window from '../lib/window.js';
+
+async function Run() {
+    const input = await Advent.GetInput();
+
+    // await Advent.Submit(null);
+    // await Advent.Submit(null, 2);
+}
+Run();
