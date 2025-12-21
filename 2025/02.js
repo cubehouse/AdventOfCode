dbe5c4c90efaddb2f59f0ba5a94dd83af5ea55d9
Jamie Holding <me@jamie.holdings>
2025-12-21 16:38:05 +0000

    [+] 2025 Day 2 Part 2

--- 2025/02.js
+++ 2025/02.js
@@ -37,6 +37,43 @@ async function Run() {
 
     await Advent.Submit(invalidSum, 1);
 
+
+    const isValid2 = (id) => {
+        const idStr = id.toString();
+        const len = idStr.length;
+
+        const seedsToTest = Math.floor(len / 2);
+
+        for(let seedLen = 1; seedLen <= seedsToTest; seedLen++) {
+            const seed = idStr.slice(0, seedLen);
+            // check if the string length is a multiple of seed length
+            if (len % seedLen !== 0) {
+                continue;
+            }
+            // check if seed repeats to make the full string
+            let repeated = '';
+            const repeatCount = len / seedLen;
+            for(let i = 0; i < repeatCount; i++) {
+                repeated += seed;
+            }
+            if (repeated === idStr) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+
+    let invalidSum2 = 0;
+    groups.forEach(([start, end]) => {
+        for (let i = start; i <= end; i++) {
+            if (!isValid2(i)) {
+                invalidSum2 += i;
+            }
+        }
+    });
+
+    await Advent.Submit(invalidSum2, 2);
     // await Advent.Submit(null, 2);
 }
 Run();
