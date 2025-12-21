df6de1fc9be904ffb6c2d160970d63a2fbaffbae
Jamie Holding <me@jamie.holdings>
2025-12-21 16:31:55 +0000

    [+] 2025 Day 2 Part 1

--- 2025/02.js
+++ 2025/02.js
@@ -7,7 +7,36 @@ const Advent = new AdventLib(2, 2025);
 async function Run() {
     const input = await Advent.GetInput();
 
-    // await Advent.Submit(null);
+    const groups = input.split(",").map((x) => {
+        return x.split("-").map(Number);
+    });
+
+    const isValid = (id) => {
+        const idStr = id.toString();
+        // skip if odd length
+        if (idStr.length % 2 !== 0) return true;
+
+        const half = idStr.length / 2;
+        const firstHalf = idStr.slice(0, half);
+        const secondHalf = idStr.slice(half);
+
+        if (firstHalf === secondHalf) {
+            return false;
+        }
+        return true;
+    };
+
+    let invalidSum = 0;
+    groups.forEach(([start, end]) => {
+        for (let i = start; i <= end; i++) {
+            if (!isValid(i)) {
+                invalidSum += i;
+            }
+        }
+    });
+
+    await Advent.Submit(invalidSum, 1);
+
     // await Advent.Submit(null, 2);
 }
 Run();
